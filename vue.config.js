99814236a45b007456b96fd0f6763bc6a645d96a
root <root@VM-APP-1.wr1aftrxjuhevnxcwupq4pxheh.cx.internal.cloudapp.net>
2024-09-20 23:12:41 +0000

    unificar ambientes

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     host: '0.0.0.0',   // Escuchar en todas las interfaces de red
     port: 8081,        // Puerto en el que se sirve la aplicación
-    https: true,       // Asegúrate de que HTTPS está habilitado
+    https: process.env.PROD === 'true',       // Asegúrate de que HTTPS está habilitado
     proxy: { 
     }
   },
